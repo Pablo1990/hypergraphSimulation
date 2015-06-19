723edf199f1377535796713b33477ecca95d98d5
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-06-19 11:06:57 +0200

    init cytoscape plugin project

---
+++ hypergraphs/src/org/cytoscape/hyper/Main.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.cytoscape.hyper;
+
+import org.cytoscape.app.AbstractCyApp;
+import org.cytoscape.app.CyAppAdapter;
+
+/**
+ * @author pablovm1990
+ *
+ */
+public class Main extends AbstractCyApp{
+
+	public Main(CyAppAdapter adapter) {
+		super(adapter);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
